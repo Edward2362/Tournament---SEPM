dd7b2842330b24977caaa2d8635918f5797b8f8a
Quang <s3817788@rmit.edu.vn>
2022-03-29 14:38:59 +0700

    Tournament

---
+++ Tournament - frontend/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">Hi this is layout</div>
+</template>
+
+<script>
+import NuxtLogo from "../components/NuxtLogo.vue";
+
+export default {
+  components: {
+    NuxtLogo,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  line-height: 1.6;
+  background: #f4f4f4;
+}
+a {
+  color: #666;
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+}
+.container {
+  max-width: 800px;
+  margin: 2rem auto;
+  overflow: hidden;
+  padding: 1rem 2rem;
+  background: #fff;
+}
+</style>
