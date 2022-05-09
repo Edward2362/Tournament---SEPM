725bf001aca78df3d30cc1454b3c0e7bddfbb3f5
tuann07 <s3817907@rmit.edu.vn>
2022-05-09 17:56:08 +0700

    test: remove customer function

--- server/app.test.js
+++ server/app.test.js
@@ -20,75 +20,60 @@ afterEach(async () => {
 });
 afterAll(() => mongoose.connection.close());
 
-const shouldRespondWith =
-  (statusCode, ...requests) =>
-  () => {
-    it(`Should respond with ${statusCode}`, async () => {
-      const mainRequest = requests.pop();
-
-      for (const request of requests) await request();
-
-      const response = await mainRequest();
-      expect(response.statusCode).toBe(statusCode);
-    });
-  };
-
-const loginUser =
-  (data, quantity = 1) =>
-  async () => {
-    try {
-      if (quantity < 1) {
-        return;
-      }
-      const usersPromises = [];
-      for (let i = 0; i < quantity; i++) {
-        usersPromises.push(
-          User.create({
-            email: `testing${i}@email.com`,
-            username: "username",
-            password: "password",
-          })
-        );
-      }
-
-      await Promise.all(usersPromises);
-
-      const loginsPromises = [];
-      for (let i = 0; i < quantity; i++) {
-        loginsPromises.push(
-          request
-            .post("/api/v1/auth/login")
-            .send({ email: `testing${i}@email.com`, password: "password" })
-        );
-      }
-
-      const responses = await Promise.all(loginsPromises);
-
-      const users = responses.map((response) => {
-        expect(response.statusCode).toBe(StatusCodes.OK);
-        return {
-          ...response.body.data,
-          token: response.body.token,
-        };
-      });
-
-      data.users = users;
-    } catch (error) {
-      console.log(error);
+const loginUser = async (data, quantity = 1) => {
+  try {
+    if (quantity < 1) {
+      return;
     }
-  };
+    const usersPromises = [];
+    for (let i = 0; i < quantity; i++) {
+      usersPromises.push(
+        User.create({
+          email: `testing${i}@email.com`,
+          username: "username",
+          password: "password",
+        })
+      );
+    }
 
-const createProject = (data) => async () => {
+    await Promise.all(usersPromises);
+
+    const loginsPromises = [];
+    for (let i = 0; i < quantity; i++) {
+      loginsPromises.push(
+        request
+          .post("/api/v1/auth/login")
+          .send({ email: `testing${i}@email.com`, password: "password" })
+      );
+    }
+
+    const responses = await Promise.all(loginsPromises);
+
+    const users = responses.map((response) => {
+      expect(response.statusCode).toBe(StatusCodes.OK);
+      return {
+        ...response.body.data,
+        token: response.body.token,
+      };
+    });
+
+    data.users = users;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createProject = async (data) => {
   try {
     const project = await Project.create({
       name: "test project",
       trelloBoardId: "trello board id",
       lastAccessed: [{ user: data.users[0].userId }],
+      admin: data.users[0].userId,
     });
     const member = await Member.create({
       user: data.users[0].userId,
       project: project._id,
-      role: "admin",
     });
 
     data.project = project;
@@ -99,250 +84,244 @@ const createProject = (data) => async () => {
 };
 
 describe("TEST /auth/register", () => {
-  describe(
-    "Register with all properties",
-    shouldRespondWith(StatusCodes.CREATED, async () =>
-      request.post("/api/v1/auth/register").send({
+  describe("Register with all properties", () => {
+    it(`should be created`, async () => {
+      const response = await request.post("/api/v1/auth/register").send({
         email: "testing@email.com",
         username: "username",
         password: "password",
-      })
-    )
-  );
+      });
 
-  describe(
-    "Register without any property",
-    shouldRespondWith(StatusCodes.BAD_REQUEST, async () =>
-      request.post("/api/v1/auth/register").send({})
-    )
-  );
+      expect(response.statusCode).toBe(StatusCodes.CREATED);
+    });
+  });
 
-  describe(
-    "Register with a registered email",
-    shouldRespondWith(
-      StatusCodes.BAD_REQUEST,
-      async () =>
-        User.create({
-          email: "testing@email.com",
-          username: "username",
-          password: "password",
-        }),
-      async () =>
-        request.post("/api/v1/auth/register").send({
-          email: "testing@email.com",
-          username: "username",
-          password: "password",
-        })
-    )
-  );
+  describe("Register without any property", () => {
+    it(`should be bad request`, async () => {
+      const response = await request.post("/api/v1/auth/register").send({});
+
+      expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    });
+  });
+
+  describe("Register with a registered email", () => {
+    it(`should be bad request`, async () => {
+      await User.create({
+        email: "testing@email.com",
+        username: "username",
+        password: "password",
+      });
+
+      const response = await request.post("/api/v1/auth/register").send({
+        email: "testing@email.com",
+        username: "username",
+        password: "password",
+      });
+
+      expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST);
+    });
+  });
 });
 
 describe("TEST /auth/login", () => {
-  describe(
-    "Login with a registered email",
-    shouldRespondWith(
-      StatusCodes.OK,
-      async () =>
-        User.create({
-          email: "testing@email.com",
-          username: "username",
-          password: "password",
-        }),
-      async () =>
-        request.post("/api/v1/auth/login").send({
-          email: "testing@email.com",
-          password: "password",
-        })
-    )
-  );
+  describe("Login with a registered email", () => {
+    it("should be ok", async () => {
+      await User.create({
+        email: "testing@email.com",
+        username: "username",
+        password: "password",
+      });
 
-  describe(
-    "Login without registering",
-    shouldRespondWith(StatusCodes.UNAUTHORIZED, async () =>
-      request.post("/api/v1/auth/login").send({
+      const response = await request.post("/api/v1/auth/login").send({
         email: "testing@email.com",
         password: "password",
-      })
-    )
-  );
+      });
 
-  describe(
-    "Login with a wrong password",
-    shouldRespondWith(
-      StatusCodes.UNAUTHORIZED,
-      async () =>
-        User.create({
-          email: "testing@email.com",
-          username: "username",
-          password: "password",
-        }),
-      async () =>
-        request.post("/api/v1/auth/login").send({
-          email: "testing@email.com",
-          password: "wrong-password",
-        })
-    )
-  );
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Login without registering", () => {
+    it("should be unathorized", async () => {
+      const response = await request.post("/api/v1/auth/login").send({
+        email: "testing@email.com",
+        password: "password",
+      });
+
+      expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+    });
+  });
+
+  describe("Login with a wrong password", () => {
+    it("should be unauthorized", async () => {
+      await User.create({
+        email: "testing@email.com",
+        username: "username",
+        password: "password",
+      });
+
+      const response = await request.post("/api/v1/auth/login").send({
+        email: "testing@email.com",
+        password: "wrong-password",
+      });
+
+      expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+    });
+  });
 });
 
 describe("TEST /users/me", () => {
   let data = {};
 
-  describe(
-    "With token",
-    shouldRespondWith(StatusCodes.OK, loginUser(data), async () =>
-      request
-        .get("/api/v1/users/me")
-        .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  describe("With token", () => {
+    it("should be ok", async () => {
+      await loginUser(data);
 
-  describe(
-    "Without token",
-    shouldRespondWith(StatusCodes.UNAUTHORIZED, async () =>
-      request.get("/api/v1/users/me")
-    )
-  );
+      const response = await request
+        .get("/api/v1/users/me")
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Without token", () => {
+    it("should be ok", async () => {
+      const response = await request.get("/api/v1/users/me");
+
+      expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+    });
+  });
 });
 
 describe("TEST /projects", () => {
   let data = {};
 
-  describe(
-    "Get all projects",
-    shouldRespondWith(StatusCodes.OK, loginUser(data), async () =>
-      request
-        .get("/api/v1/users/me/projects")
-        .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  beforeEach(async () => await loginUser(data));
 
-  describe(
-    "Create a project",
-    shouldRespondWith(StatusCodes.CREATED, loginUser(data), async () =>
-      request
+  describe("Get all projects", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .get("/api/v1/users/me/projects")
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Create a project", () => {
+    it("should be created", async () => {
+      const response = await request
         .post("/api/v1/projects")
         .send({
           name: "project",
           trelloBoardId: "id",
         })
-        .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+        .set("Authorization", "Bearer " + data.users[0].token);
 
-  describe(
-    "Get a project",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .get("/api/v1/projects/" + data.project._id)
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+      expect(response.statusCode).toBe(StatusCodes.CREATED);
+    });
+  });
 
-  describe(
-    "Update a project",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .patch("/api/v1/projects/" + data.project._id)
-          .send({
-            name: "project-update",
-            trelloBoardId: "id",
-          })
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  beforeEach(async () => await createProject(data));
 
-  describe(
-    "Delete a project",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .delete("/api/v1/projects/" + data.project._id)
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  describe("Get a project", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .get("/api/v1/projects/" + data.project._id)
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Update a project", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .patch("/api/v1/projects/" + data.project._id)
+        .send({
+          name: "project-update",
+          trelloBoardId: "id",
+        })
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Delete a project", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .delete("/api/v1/projects/" + data.project._id)
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
 });
 
 describe("TEST /members", () => {
   let data = {};
 
-  describe(
-    "Get all members",
-    shouldRespondWith(StatusCodes.OK, loginUser(data), async () =>
-      request
+  beforeEach(async () => {
+    await loginUser(data, 2);
+    await createProject(data);
+  });
+
+  describe("Get all members", () => {
+    it("should be ok", async () => {
+      const response = await request
         .get("/api/v1/users/me/members")
-        .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+        .set("Authorization", "Bearer " + data.users[0].token);
 
-  describe(
-    "Create a member",
-    shouldRespondWith(
-      StatusCodes.CREATED,
-      loginUser(data, 2),
-      createProject(data),
-      async () => {
-        return await request
-          .post(`/api/v1/projects/${data.project._id.toString()}/members`)
-          .send({
-            userId: data.users[1].userId,
-          })
-          .set("Authorization", "Bearer " + data.users[0].token);
-      }
-    )
-  );
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
 
-  describe(
-    "Get a member",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .get("/api/v1/members/" + data.member._id)
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  describe("Create a member", () => {
+    it("should be created", async () => {
+      const response = await request
+        .post(`/api/v1/projects/${data.project._id.toString()}/members`)
+        .send({
+          userId: data.users[1].userId,
+        })
+        .set("Authorization", "Bearer " + data.users[0].token);
 
-  describe(
-    "Update a member",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .patch("/api/v1/members/" + data.member._id)
-          .send({
-            name: "project-update",
-            trelloBoardId: "id",
-          })
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+      expect(response.statusCode).toBe(StatusCodes.CREATED);
+    });
+  });
 
-  describe(
-    "Delete a member",
-    shouldRespondWith(
-      StatusCodes.OK,
-      loginUser(data),
-      createProject(data),
-      async () =>
-        request
-          .delete("/api/v1/members/" + data.member._id)
-          .set("Authorization", "Bearer " + data.users[0].token)
-    )
-  );
+  describe("Get a member", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .get("/api/v1/members/" + data.member._id)
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Update a member", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .patch("/api/v1/members/" + data.member._id)
+        .send({
+          name: "project-update",
+          trelloBoardId: "id",
+        })
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
+
+  describe("Delete a member", () => {
+    it("should be ok", async () => {
+      const response = await request
+        .delete("/api/v1/members/" + data.member._id)
+        .set("Authorization", "Bearer " + data.users[0].token);
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+    });
+  });
 });
